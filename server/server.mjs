5d5c2cf508aebba1b951af2217a910006462a09b
anmol <anmolsinha4321@gmail.com>
2025-03-26 20:31:45 +0530

    added REACT_APP_API_URL in .env file

--- server/server.mjs
+++ server/server.mjs
@@ -11,7 +11,12 @@ import { generateToken, verifyToken } from './middlewares/middleware.js';
 import path from 'path';
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://wandercall.onrender.com',
+  credentials: true,
+}));
+
 app.use(express.json());
 dotenv.config();
 
@@ -25,6 +30,7 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING)
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
+  requireTLS: true,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
@@ -446,6 +452,6 @@ app.get('*', (_, res) => {
 });
 
 // Start the server
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
   console.log('Server is running on localhost:5000');
 });
