3dfcd5730aff680310ff8c1964c821d87b110bdf
anmol <anmolsinha4321@gmail.com>
2025-03-27 21:52:21 +0530

    added minify: false to prevent eval

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     'process.env': process.env, 
   },
   build: {
-    chunkSizeWarningLimit: 1000, // Set limit in KB (e.g., 1000 KB = 1 MB)
+    chunkSizeWarningLimit: 1000,
+    minify: false,
   },
 });
