d85ebe66cfd512968472dc55b331db4303f635ed
anmol <anmolsinha4321@gmail.com>
2025-03-30 21:15:16 +0530

    change path of footer.css

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 
-import './Footer.css';
+import './footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faTwitter, faFacebook, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faPhone, faMapMarkerAlt, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
