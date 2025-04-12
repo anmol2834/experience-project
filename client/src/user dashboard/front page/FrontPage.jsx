a86bc0abe4201a152f2fbeaa9d9694404baad3fc
anmol <anmolsinha4321@gmail.com>
2025-04-13 01:28:13 +0530

    created menu bar

--- client/src/user dashboard/front page/FrontPage.jsx
+++ client/src/user dashboard/front page/FrontPage.jsx
@@ -10,7 +10,6 @@ import HomePage from '../home page/HomePage';
 import MenuBar from '../menu-bar/MenuBar';
 import Footer from '../../components/footer/Footer';
 
-
 const slides = [slide1, slide2, slide3, slide4];
 
 const FrontPage = () => {
@@ -21,7 +20,6 @@ const FrontPage = () => {
   const { token } = useAuth();
   const homePageRef = useRef(null);
 
-
   useEffect(() => {
     const scrollToProductId = location.state?.scrollToProductId;
     const fallbackScrollPosition = location.state?.fallbackScrollPosition || 0;
@@ -31,7 +29,6 @@ const FrontPage = () => {
       if (targetCard) {
         targetCard.scrollIntoView({ behavior: 'smooth', block: 'center' });
       } else {
-
         setTimeout(() => {
           window.scrollTo({ top: fallbackScrollPosition, behavior: 'smooth' });
         }, 500);
@@ -39,7 +36,6 @@ const FrontPage = () => {
     } else {
       window.scrollTo({ top: fallbackScrollPosition, behavior: 'smooth' });
     }
-
 
     if (location.state?.scrollToProductId || location.state?.fallbackScrollPosition) {
       navigate('/', { replace: true, state: {} });
@@ -97,7 +93,7 @@ const FrontPage = () => {
       const scrollPosition = window.scrollY;
       const windowHeight = document.documentElement.scrollHeight - window.innerHeight;
 
-      if (scrollPosition > windowHeight * 0) {
+      if (scrollPosition > windowHeight * 0.09) {
         setShowMenu(true);
       } else {
         setShowMenu(false);
@@ -121,34 +117,44 @@ const FrontPage = () => {
           <li></li>
         </ul>
       </div>
-    )
+    );
   }
 
   return (
-    <div className='front-contain'>
+    <div className="front-contain">
       <div className="headers">
-        <div className="logo">
-          Wandercall
-        </div>
-
+        <div className="logo">Wandercall</div>
         <div className="app-info-menu">
           <span>Experience</span>
           <span>About Us</span>
           <span>Contact</span>
           <span>FAQ</span>
         </div>
-
         <div className="header-btn">
           {token ? (
-            <span style={{ display: 'block' }} className='user-icon' onClick={() => navigate('/account')}>
-              <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#fff">
+            <span
+              style={{ display: 'block' }}
+              className="user-icon"
+              onClick={() => navigate('/account')}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="50px"
+                viewBox="0 -960 960 960"
+                width="50px"
+                fill="#fff"
+              >
                 <path d="M240.92-268.31q51-37.84 111.12-59.77Q412.15-350 480-350t127.96 21.92q60.12 21.93 111.12 59.77 37.3-41 59.11-94.92Q800-417.15 800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 62.85 21.81 116.77 21.81 53.92 59.11 94.92ZM480.01-450q-54.78 0-92.39-37.6Q350-525.21 350-579.99t37.6-92.39Q425.21-710 479.99-710t92.39 37.6Q610-634.79 610-580.01t-37.6 92.39Q534.79-450 480.01-450ZM480-100q-79.15 0-148.5-29.77t-120.65-81.08q-51.31-51.3-81.08-120.65Q100-400.85 100-480t29.77-148.5q29.77-69.35 81.08-120.65 51.3-51.31 120.65-81.08Q400.85-860 480-860t148.5 29.77q69.35 29.77 120.65 81.08 51.31 51.3 81.08 120.65Q860-559.15 860-480t-29.77 148.5q-29.77 69.35-81.08 120.65-51.3 51.31-120.65 81.08Q559.15-100 480-100Zm0-60q54.15 0 104.42-17.42 50.27-17.43 89.27-48.73-39-30.16-88.11-47Q536.46-290 480-290t-105.77 16.65q-49.31 16.66-87.92 47.2 39 31.3 89.27 48.73Q425.85-160 480-160Zm0-350q29.85 0 49.92-20.08Q550-550.15 550-580t-20.08-49.92Q509.85-650 480-650t-49.92 20.08Q410-609.85 410-580t20.08 49.92Q450.15-510 480-510Zm0-70Zm0 355Z" />
               </svg>
             </span>
           ) : (
             <>
-              <button className="sign-btn signin" onClick={() => navigate('/signin')}>sign in</button>
-              <button className="sign-btn signup" onClick={() => navigate('/signup')}>sign up</button>
+              <button className="sign-btn signin" onClick={() => navigate('/signin')}>
+                sign in
+              </button>
+              <button className="sign-btn signup" onClick={() => navigate('/signup')}>
+                sign up
+              </button>
             </>
           )}
         </div>
@@ -165,7 +171,13 @@ const FrontPage = () => {
             />
           ))}
           <button className="explore" onClick={handleExploreClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#000000"
+            >
               <path d="M80-240v-480h80v480H80Zm560 0-57-56 144-144H240v-80h487L584-664l56-56 240 240-240 240Z" />
             </svg>
             <span>explore</span>
@@ -181,14 +193,12 @@ const FrontPage = () => {
           ))}
         </div>
       </div>
-      <div className='homepage' ref={homePageRef}>
+      <div className="homepage" ref={homePageRef}>
         <HomePage />
       </div>
-
       <div>
         <Footer homeRef={homePageRef} />
       </div>
-
       <div className={`menu-div ${showMenu ? "show" : "hide"}`}>
         <MenuBar />
       </div>
--- client/src/user dashboard/menu-bar/MenuBar.jsx
+++ client/src/user dashboard/menu-bar/MenuBar.jsx
@@ -1,11 +1,100 @@
-
+import { useState, useEffect } from "react";
+import { 
+  faCommentAlt, 
+  faGift, 
+  faHeart, 
+  faList, 
+  faTicket, 
+  faUser,
+  faCrown,
+  faStore
+} from "@fortawesome/free-solid-svg-icons";
 import "./menubar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from "react-router-dom";
 
 const Menubar = () => {
-  
+  const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.matchMedia("(max-width: 992px)").matches);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const mobileView = window.matchMedia("(max-width: 992px)").matches;
+      setIsMobile(mobileView);
+      if (!mobileView) setMenuOpen(false);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const toggleMenu = () => isMobile && setMenuOpen(!menuOpen);
+
+  useEffect(() => {
+    document.body.style.overflow = (menuOpen && isMobile) ? "hidden" : "auto";
+    return () => document.body.style.overflow = "auto";
+  }, [menuOpen, isMobile]);
 
   return (
     <div className="menu-bar-contain">
+      <div className="menu-container">
+        <FontAwesomeIcon
+          icon={faList}
+          className="hamburger-icon"
+          onClick={toggleMenu}
+        />
+        <div className={`menu-items ${menuOpen ? "show" : ""}`}>
+          <div className="menu-type1">
+            <span className="mobile-menu-item">
+              <FontAwesomeIcon icon={faHeart} className="menu-icon" />
+              <p className="menu-label">Wishlist</p>
+            </span>
+            <span className="mobile-menu-item">
+              <FontAwesomeIcon icon={faGift} className="menu-icon" />
+              <p className="menu-label">Rewards</p>
+            </span>
+            <span className="mobile-menu-item">
+              <FontAwesomeIcon icon={faTicket} className="menu-icon" />
+              <p className="menu-label">Bookings</p>
+            </span>
+            <span className="mobile-menu-item">
+              <FontAwesomeIcon icon={faCommentAlt} className="menu-icon" />
+              <p className="menu-label">Community</p>
+            </span>
+          </div>
+
+          <div className="menu-type2">
+            <button className="premimum">
+              <FontAwesomeIcon icon={faCrown} className="button-icon" />
+              Premium Card
+            </button>
+            <button className="become-provider">
+              <FontAwesomeIcon icon={faStore} className="button-icon" />
+              Become Provider
+            </button>
+          </div>
+
+          <div className="menu-type3">
+            <span>About Us</span>
+            <span>Contact</span>
+            <span>FAQ</span>
+            <span>Privacy Policy</span>
+            <span>Terms of Service</span>
+            <span className="logout-button">Logout</span>
+          </div>
+        </div>
+      </div>
+      <div className="search-bar-container">
+        <input
+          type="search"
+          placeholder="Search Your Experience"
+          className="search-bar"
+        />
+      </div>
+      <div className="user-icon-container" onClick={() => navigate("/account")}>
+        <FontAwesomeIcon icon={faUser} size="2x" />
+      </div>
     </div>
   );
 };
